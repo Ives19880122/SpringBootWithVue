638749f84422ece1f3ef7b86904318d45fd498ec
Ives <jianxing.imo99g@gmail.com>
2023-06-07 00:28:59 +0800

    fix: 測試追加dev contianer in vscode

--- server/src/main/java/com/example/demo/controller/HomeController.java
+++ server/src/main/java/com/example/demo/controller/HomeController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Slf4j
 @Controller
@@ -17,5 +18,12 @@ public class HomeController {
     @GetMapping("vue/page*")
     public String vuePage(){
         return "forward:/index.html";
+    }
+
+    @ResponseBody
+    @GetMapping
+    public String hello(){
+        log.info("有進來");
+        return "hello";
     }
 }
